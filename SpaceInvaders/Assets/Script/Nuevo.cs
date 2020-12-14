45afde523ffe78d57abf76f84ab0cc639ee4fc27
Fernando Matías Cruz <matias18233@gmail.com>
2020-12-14 18:04:36 -0300

    Modificaciones sobre el proyecto

---
+++ SpaceInvaders/Assets/Script/Nuevo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nuevo : MonoBehaviour {
+    void Start() {
+        // Inicialización de la clase
+        Debug.Log("Estoy con vida!");
+    }
+
+    void Update() {
+        // Actualización de cada Frame
+    }
+}
