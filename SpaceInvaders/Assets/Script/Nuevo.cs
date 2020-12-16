d7b996c131810a4b1cf0621041d217e39d209f4e
Fernando Matías Cruz <matias18233@gmail.com>
2020-12-16 18:26:14 -0300

    Agregado de movimientos de la nave

--- SpaceInvaders/Assets/Script/Nuevo.cs
+++ SpaceInvaders/Assets/Script/Nuevo.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Nuevo : MonoBehaviour {
+
+    [Header("Valores principales")]
+    public string myName;
+    
+    [Header("Variables numéricas")]
+    [Range(1, 6)]
+    public int numero;
+    [Range(0.2f, 0.8f)]
+    public float valor;
+
     void Start() {
         // Inicialización de la clase
-        Debug.Log("Estoy con vida!");
+        Debug.Log("I am alive and my name is " + myName);
     }
 
     void Update() {
---
+++ SpaceInvaders/Assets/Script/ShipControlle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControlle : MonoBehaviour {
+    private new Rigidbody2D rigidbody;
+    private Vector2 movement;
+    private Vector2 movementX;
+    public float speed = 1f;
+
+    void Start() { // Se llama a Start antes de la primera actualización del cuadro
+        rigidbody = GetComponent<Rigidbody2D>();
+        int posicionX = Random.Range(-100, 100);
+        Debug.Log("Posición inicial: " + posicionX);
+        movement = new Vector2(posicionX, 0);
+        rigidbody.AddForce(movement);
+        // Debug.Log(x);
+    }
+
+    void Update() { // Update se llama una vez por frame
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        movement = new Vector2(moveHorizontal, moveVertical);
+    }
+    
+    void FixedUpdate() { // FixedUpdate se llama en cada fixed frame-rate frame. (50 llamadas por segundo, por defecto)
+        // Aplica la fuerza al Rigidbody2d
+        rigidbody.AddForce(movement * speed * 5f);
+    }
+}
